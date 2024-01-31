10f4883e0871636b074478871c9b1813b3a38f09
zZeTraL <lucashbs.pro@gmail.com>
2024-01-31 23:59:26 +0100

    Update

---
+++ functions/utils.py
@@ -0,0 +1,49 @@
+import os
+import pandas as pd
+from PIL import Image
+
+
+def convert_size(size_bytes):
+    if size_bytes == 0:
+        return "0 B"
+    units = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
+    i = 0
+    while size_bytes >= 1024 and i < len(units) - 1:
+        size_bytes /= 1024.0
+        i += 1
+    return f"{round(size_bytes, 2)} {units[i]}"
+
+
+def get_files(input_folder):
+    # Retrieve every file in the input_folder
+    files_name = os.listdir(input_folder)
+    files_path = []
+    for name in files_name:
+        files_path.append(os.path.join(input_folder, name).replace("\\", "/"))
+    return files_name, files_path
+
+
+def get_files_to_compress(input_folder, log):
+    files_name, files_path = get_files(input_folder)
+    files_to_compress = []
+    files_already_compressed = []
+
+    print(f"[INFO] Files in the input folder: {len(files_path)}")
+
+    for path in files_path:
+        # Check if the file is already compressed
+        if path in log["path"].values:
+            files_already_compressed.append(path)
+        else:
+            files_to_compress.append(path)
+
+    print(f"[INFO] Files already compressed: {len(files_path) - len(files_to_compress)}")
+    # DEBUG
+    #for path in files_already_compressed:
+        #print(f"{os.path.basename(path)}")
+    print(f"[INFO] Files to compress: {len(files_to_compress)}")
+    # DEBUG
+    #for path in files_to_compress:
+        #print(f"{os.path.basename(path)}")
+    return files_to_compress
+
--- main.py
+++ main.py
@@ -1,111 +1,90 @@
-import os
-import shutil
+from concurrent.futures import ThreadPoolExecutor
 
 import pandas as pd
-from concurrent.futures import ThreadPoolExecutor
+import concurrent.futures
 from PIL import Image
-from tqdm import tqdm
+from functions.utils import *
+from functions.log import *
 
 # Constants
-THREAD_NUMBER = 6
-BATCH_SIZE = 16
-START_KEYWORD = "StarRailRes"
+THREAD_NUMBER = 4
+BATCH_SIZE = 10
 
 
-def convert_size(size_bytes):
-    if size_bytes == 0:
-        return "0B"
-    units = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
-    i = 0
-    while size_bytes >= 1024 and i < len(units) - 1:
-        size_bytes /= 1024.0
-        i += 1
-    return f"{round(size_bytes, 2)} {units[i]}"
+def compress_file(input_path, output_path, compression_level):
+    # We retrieve the name of the file
+    file_name = os.path.basename(input_path)
 
+    # We open the image and save it with the compression level to the output folder
+    image = Image.open(input_path)
+    image.save(output_path, optimize=True, quality=compression_level)
 
-def compress(file_path, output_path, quality):
-    image = Image.open(file_path)
-    original_size = os.path.getsize(file_path)
-    image.save(output_path, optimize=True, quality=quality)
+    # We compute the original size, the compressed size, the saved storage and the percent
+    original_size = os.path.getsize(input_path)
     compressed_size = os.path.getsize(output_path)
-    storage_saved = original_size - compressed_size
-    percent = str(round((storage_saved / original_size) * 100, 2)) + "%"
+    saved_storage = original_size - compressed_size
+    percent = str(round((saved_storage / original_size) * 100, 2)) + "%"
+    is_compressed = True
 
-    return original_size, compressed_size, storage_saved, percent
+    # If compressed size is greater than original size, we delete the compressed file
+    if compressed_size > original_size:
+        os.remove(output_path)
+        print(f"[INFO] {file_name} was not compressed because the compressed size is greater than the original size")
+        return input_path, file_name, original_size, original_size, 0, 0, None, False
 
-
-def get_files_to_compress(input_folder, log):
-    files_to_compress = []
-    for root, dirs, files in os.walk(input_folder):
-        for file in files:
-            path = os.path.join(root, file).replace("\\", "/")
-            if path not in log["path"].values:
-                files_to_compress.append(path)
-    return files_to_compress
+    return input_path, file_name, original_size, compressed_size, saved_storage, percent, output_path, is_compressed
 
 
-def process_batch(batch, current_dir, output_folder, quality, df, log):
-    with ThreadPoolExecutor(max_workers=THREAD_NUMBER) as executor:
+def process_batch(batch, output_folder, compression_level):
+    with concurrent.futures.ThreadPoolExecutor(max_workers=THREAD_NUMBER) as executor:
         futures = []
-        for path in batch:
-            file_name = path[path.rfind("/") + 1:]
-            output_file = os.path.join(output_folder, file_name).replace("\\", "/")
+        for file in batch:
+            output_path = os.path.join(output_folder, os.path.basename(file)).replace("\\", "/")
+            future = executor.submit(compress_file, file, output_path, compression_level)
+            futures.append(future)
 
-            future = executor.submit(compress, path, output_file, quality)
-            futures.append((path, future))
-
-        for path, future in futures:
-            original_size, compressed_size, storage_saved, percent = future.result()
-            df.loc[len(df)] = [current_dir, file_name,
+        for future in concurrent.futures.as_completed(futures):
+            input_path, name, original_size, compressed_size, saved_storage, percent, output_path, is_compressed = future.result()
+            df.loc[len(df)] = [input_path, name,
                                convert_size(original_size),
                                convert_size(compressed_size),
-                               convert_size(storage_saved),
+                               convert_size(saved_storage),
                                percent,
-                               path]
-            log.loc[len(log)] = [path]
+                               output_path]
+            log.loc[len(log)] = [input_path, is_compressed, compression_level]
+
+    print(f"[INFO] Batch of {len(batch)} files compressed")
 
 
-def main(input_folder, output_folder, quality=80, overwrite=False):
-
-    if not os.path.exists("log.csv"):
-        pd.DataFrame(columns=["path"]).to_csv("log.csv", index=False)
-
-    if not overwrite:
-        if os.path.exists(output_folder):
-            shutil.rmtree(output_folder)
-            os.mkdir(output_folder)
-        else:
-            os.mkdir(output_folder)
-    else:
-        output_folder = input_folder;
-
-    log = pd.DataFrame(pd.read_csv("log.csv"))
-    df = pd.DataFrame(columns=["folder", "file_name", "original_size", "compressed_size", "storage_saved", "percent", "path"])
-
+def main(input_folder, output_folder, compression_level, overwrite):
+    # Check if the output folder exists and overwrite is False
+    if not os.path.exists(output_folder) and not overwrite:
+        os.mkdir(output_folder)
+    # We retrieve the files to compress
     files_to_compress = get_files_to_compress(input_folder, log)
-
-    if not files_to_compress:
-        print("[INFO] No files to compress")
+    if len(files_to_compress) == 0:
+        print(f"[INFO] Operation done : No files to compress")
         return
 
-    print(f"[INFO] Already compressed files: {len(log)}")
-    print(f"[INFO] Files to compress: {len(files_to_compress)}")
+    # We create a list of batches of files to compress
+    batches = []
+    for i in range(0, len(files_to_compress), BATCH_SIZE):
+        batches.append(files_to_compress[i:i + BATCH_SIZE])
 
-    current_dir = input_folder[input_folder.rfind(START_KEYWORD):]
-    batched_files = [files_to_compress[i:i + BATCH_SIZE] for i in range(0, len(files_to_compress), BATCH_SIZE)]
+    for batch in batches:
+        process_batch(batch, output_folder, compression_level)
 
-    print("[INFO] Compressing files...")
-    for batch in tqdm(batched_files, desc="Batch progression"):
-        process_batch(batch, current_dir, output_folder, quality, df, log)
-
-    print("[INFO] Saving report...")
-    log.to_csv("log.csv", index=False)
-    date = pd.Timestamp.now().strftime("%Y-%m-%d_%H-%M-%S")
-    df.to_csv(f"compression_report_{date}.csv", index=False)
-    print("[INFO] Operation done")
-    print(f"[INFO] Total compressed files: {len(df)}")
+    print(f"[INFO] Compression finished")
+    print(f"[INFO] Files compressed: {len(df)}")
+    print(f"[INFO] Files not compressed: {len(files_to_compress) - len(df)}")
+    save_log(log)
+    save_compress_report(df)
 
 
-# Example usage
-input_directory = "D:/WebstormProjects/7Numby/client/public/StarRailRes/image/character_portrait"
-main("input", "output", 50, True)
+# Main
+input_directory = "D:/WebstormProjects/7Numby/client/public/StarRailRes/image/character_preview"
+log = load_log()
+df = create_compress_report()
+main("input", "output", 70, False)
+
+# %%
